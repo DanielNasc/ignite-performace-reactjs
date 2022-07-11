d54717d4c35dfeb1653313d964a0a1887d3708a1
DanielNasc <danienasc15987@gmail.com>
2022-07-11 19:54:05 -0300

    bundle analyzer

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
+import { List, ListRowRenderer } from "react-virtualized";
 
 interface SearchResultProps {
   results: Array<{
@@ -19,17 +20,36 @@ export function SearchResult({
   onAddToWishList,
   totalPrice,
 }: SearchResultProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div id="resuts">
       <h2>total: {totalPrice}</h2>
 
-      {results.map((result) => (
+      <List
+        height={300}
+        width={900}
+        rowHeight={30}
+        overscanRowCount={10}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {/* {results.map((result) => (
         <ProductItem
           key={result.id}
           product={result}
           onAddToWishList={onAddToWishList}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
